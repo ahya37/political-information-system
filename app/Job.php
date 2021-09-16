d00c91e6d8dfdc6e726e2c5bbfd31dfaa3ea5ee8
ahya37 <ahmadyaniid37@gmail.com>
2021-09-16 13:16:22 +0700

    grafik jobs dashboard nasional v.2

--- app/Job.php
+++ app/Job.php
@@ -61,15 +61,11 @@ class Job extends Model
 
     public function getMostJobs()
     {
-        $sql = "SELECT e.name, COUNT(e.name) as total_job
-				from regencies as a
-                join districts as b on a.id = b.regency_id
-                join villages as c on b.id = c.district_id
-                join users as d on c.id = d.village_id
-                join jobs as e on d.job_id = e.id
-                join regencies as f on b.regency_id = f.id
-                GROUP by e.name
-                order by COUNT(e.name) desc
+        $sql = "SELECT b.name, COUNT(b.name) as total_job
+                from users as a
+                join jobs as b on a.job_id = b.id 
+                GROUP by b.name
+                order by COUNT(b.name) desc
                 limit 6";
         return DB::select($sql);
     }
