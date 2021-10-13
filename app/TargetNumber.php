5fa08eaf174fe29ec8e0bfa85a6a63d2ab0684eb
ahya37 <ahmadyaniid37@gmail.com>
2021-10-13 14:57:20 +0700

    target dimanis

--- app/TargetNumber.php
+++ app/TargetNumber.php
@@ -11,8 +11,7 @@ class TargetNumber extends Model
 
     public function getTotalTargetMemberNational()
     {
-        $sql = "SELECT sum(a.target) as target FROM target_numbers as a
-                join districts as b on a.district_id = b.id
+        $sql = "SELECT sum(b.target) as target FROM districts as b
                 join regencies as c on b.regency_id = c.id
                 join provinces as d on c.province_id = d.id";
         $result = collect(\ DB::select($sql))->first();
