962fae64e1a75f71258a61b5e202dada703cad5a
ahya37 <ahmadyaniid37@gmail.com>
2021-12-09 13:36:59 +0700

    final add peserta pada event

--- resources/views/pages/admin/event/add-participant.blade.php
+++ resources/views/pages/admin/event/add-participant.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-@section('title','Daftar Event')
+@section('title','Daftar Event - Tambah Peserta')
 @push('addon-style')
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.24/datatables.min.css"/>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
@@ -15,7 +15,7 @@
           >
             <div class="container-fluid">
               <div class="dashboard-heading">
-                <h2 class="dashboard-title">Daftar Event</h2>
+                <h2 class="dashboard-title">Tambah Peserta Event</h2>
                 <p class="dashboard-subtitle">
                 </p>
               </div>
