9db7eca5886b394deb2c0733501e1d09f238d0a2
ahya37 <ahmadyaniid37@gmail.com>
2022-05-17 07:50:56 +0700

    edit format number di dashboard kecamatan

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -322,7 +322,7 @@ class DashboardController extends Controller
             'total_village' => $gF->decimalFormat($total_village),
             'total_village_filled' => $gF->decimalFormat($total_village_filled),
             'presentage_village_filled' => $presentage_village_filled,
-            'total_member' => $total_member,
+            'total_member' => $gF->decimalFormat($total_member),
             'target_member' => $gF->decimalFormat($target_member),
             'persentage_target_member' => $persentage_target_member
         ];
