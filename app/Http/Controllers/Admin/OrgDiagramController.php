ce8d52deeedfa918e25ab57b587596e847f86369
ahya37 <ahmadyaniid37@gmail.com>
2023-03-08 15:06:30 +0700

    orderable rt

--- app/Http/Controllers/Admin/OrgDiagramController.php
+++ app/Http/Controllers/Admin/OrgDiagramController.php
@@ -863,7 +863,10 @@ class OrgDiagramController extends Controller
 
           $recordsFiltered = $data->get()->count();
           if($request->input('length')!=-1) $data = $data->skip($request->input('start'))->take($request->input('length'));
-          $data = $data->orderBy($orderBy,$request->input('order.0.dir'))->get();
+        
+          $data = $data->orderBy('a.rt','asc');
+          $data = $data->orderBy($orderBy,$request->input('order.0.dir'));
+          $data = $data->get();
           
           $recordsTotal = $data->count();
 
