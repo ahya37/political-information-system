d749f7b2a9df0f06cc0a313cdf63f77484bef922
ahya37 <ahmadyaniid37@gmail.com>
2023-05-31 15:56:05 +0700

    edit flow input tps

--- app/Http/Controllers/Admin/TpsController.php
+++ app/Http/Controllers/Admin/TpsController.php
@@ -8,6 +8,11 @@ use App\Models\Regency;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\Village;
+use App\RightChooseDistrict;
+use App\RightChooseProvince;
+use App\RightChooseRegency;
+use App\RightChosseVillage;
 
 class TpsController extends Controller
 {
@@ -83,24 +88,66 @@ class TpsController extends Controller
         try {
             
             $this->validate($request, [
-                'tpnumber' => 'required',
+                'tpnumber' => 'required|min:1',
             ]);
+
+            $tpsModel  = new Tps();
+            $villageid = $request->village_id;
+
+            #cek jika data tps sudah ada dengan villageId tersebut
+            $cekTps   = $tpsModel->where('village_id', $villageid)->count();
+            $village  = Village::with(['district.regency.province'])->where('id', $villageid)->first();
 
             #hitung angka ari tps_number
             $countTpsNumber = $request->tpnumber;
 
-            for ($i= 1; $i <= $countTpsNumber ; $i++) { 
+            #jika sudah ada, maka replace data, karena data tersimpan sesuai jumlah input tps
+            if ($cekTps > 0) {
                 
-                Tps::create([
-                    'village_id' => $request->village_id,
-                    'tps_number' => $i,
-                    // 'rt' => $request->rt,
-                    // 'rw' => $request->rw
-                ]);
+                #replace
+                $tpsModel->where('village_id', $villageid)->delete();
+
+                for ($i= 1; $i <= $countTpsNumber ; $i++) { 
+                    
+                    Tps::create([
+                        'village_id' => $villageid,
+                        'tps_number' => $i,
+                    ]);
+                }
+
+            }else{
+
+                #else
+                #simpan baru
+
+                for ($i= 1; $i <= $countTpsNumber ; $i++) { 
+                    
+                    Tps::create([
+                        'village_id' => $villageid,
+                        'tps_number' => $i,
+                    ]);
+                }
+
             }
 
+            $rightChooseVillageModel  = new RightChosseVillage();
+            $rightChooseDistrictModel = new RightChooseDistrict();
+            $rightChooseRegencyModel  = new RightChooseRegency();
+            $rightChooseProvinceModel = new RightChooseProvince();
+
+            #update TPS village
+            $this->setUpdateTpsVillage($rightChooseVillageModel, $villageid,$countTpsNumber);
+
+            #update TPS district
+            $this->setUpdateTpsDistrict($rightChooseVillageModel,$rightChooseDistrictModel,$village);
+
+            #update TPS regency
+            $this->setUpdateTpsRegency($rightChooseDistrictModel,$rightChooseRegencyModel,$village);
+            
+            #update TPS province
+            $this->setUpdateTpsProvince($rightChooseRegencyModel,$rightChooseProvinceModel,$village);
+
             DB::commit();
-    
             return redirect()->back()->with(['success' => 'TPS berhasil tersimpan!']);
 
             DB::commit();
@@ -110,6 +157,53 @@ class TpsController extends Controller
 
         }
 
+
+    }
+
+    public function setUpdateTpsProvince($rightChooseRegencyModel,$rightChooseProvinceModel,$village){
+
+         #hitung count_tps regency
+         $countTpsRegency         = $rightChooseRegencyModel->where('province_id', $village->district->regency->province->id)->sum('count_tps');
+         #update count_tps di tb right_to_choose_provinces
+         $rightChooseProvince      = $rightChooseProvinceModel->where('province_id', $village->district->regency->province->id)->first();
+         if($rightChooseProvince  != null) $rightChooseProvinceModel->where('province_id', $village->district->regency->province->id)->update(['count_tps' => $countTpsRegency]);
+         return;
+
+    }
+
+    public function setUpdateTpsRegency($rightChooseDistrictModel,$rightChooseRegencyModel,$village){
+
+         #hitung count_tps district
+         $countTpsDistrict         = $rightChooseDistrictModel->where('regency_id', $village->district->regency->id)->sum('count_tps');
+         #update count_tps di tb right_to_choose_regencies
+
+         $rightChooseRegency      = $rightChooseRegencyModel->where('regency_id', $village->district->regency->id)->first();
+         if($rightChooseRegency  != null) $rightChooseRegencyModel->where('regency_id', $village->district->regency->id)->update(['count_tps' => $countTpsDistrict]);
+         return;
+
+
+    }
+
+    public function setUpdateTpsDistrict($rightChooseVillageModel,$rightChooseDistrictModel,$village){
+
+         #hitung count_tps village
+         $countTpsVillage = $rightChooseVillageModel->where('district_id', $village->district->id)->sum('count_tps');
+
+         #update count_tps di tb right_to_choose_districts
+         $rightChooseDistrict      = $rightChooseDistrictModel->where('district_id', $village->district->id)->first();
+         if($rightChooseDistrict  != null) $rightChooseDistrictModel->where('district_id', $village->district->id)->update(['count_tps' => $countTpsVillage]);
+         return;
+
+    }
+
+    public function setUpdateTpsVillage($rightChooseVillageModel, $villageid,$countTpsNumber){
+
+        #hitung jumlah tps
+        #get right_to_choose_village where $villageid
+        #update count_tps di tb right_to_choose_village
+        $rightChooseVillage      = $rightChooseVillageModel->where('village_id', $villageid)->first();
+        if($rightChooseVillage  != null) $rightChooseVillageModel->where('village_id', $villageid)->update(['count_tps' => $countTpsNumber]);
+        return;
 
     }
 
