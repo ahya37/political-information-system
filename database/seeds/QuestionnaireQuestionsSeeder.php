9e68646e93a5e8c20ca247cd357666e521046f8d
ahya37 <ahmadyaniid37@gmail.com>
2023-06-22 10:48:30 +0700

    fitur kuisioner v1.0

---
+++ database/seeds/QuestionnaireQuestionsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\QuestionnaireQuestion;
+use Illuminate\Database\Seeder;
+
+class QuestionnaireQuestionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['questionnaire_title_id' => 1, 'number' => 1,'desc' => 'Apakah Anda mendukung H.Asep AW maju sebagai DPR Provinsi Banten','required' => 'Y','created_by' => 35]
+        ];
+
+        foreach ($data as $value) {
+            QuestionnaireQuestion::create($value);
+        }
+    }
+}
