b4b879c9de834c9ab4b9c6170afb7609a2638f72
ahya37 <ahmadyaniid37@gmail.com>
2023-07-04 10:04:05 +0700

    Revisi Modul Kategori Jawaban

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -300,12 +300,22 @@
                     <a href="{{ route('admin-questionnaire') }}"
                         class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('questionnaire') ? 'active custom-active-color' : '' }}"><span>
                             Daftar Kuisioner</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('admin-questionnaire-create') }}"
-                        class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('familygroup') ? 'active custom-active-color' : '' }}"><span>
-                            Buat Kuisioner</span></a>
-                </li>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('admin-questionnaire-create') }}"
+                            class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('familygroup') ? 'active custom-active-color' : '' }}"><span>
+                                Buat Kuisioner</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin-answercategory') }}"
+                                    class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('answer_choice_categories') ? 'active custom-active-color' : '' }}"><span>
+                                        Daftar Kategori Jawaban</span></a>
+                            </li>
+                    <li class="nav-item">
+                        <a href="{{ route('admin-create-answercategory') }}"
+                            class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('familygroup') ? 'active custom-active-color' : '' }}"><span>
+                                Buat Kategori Jawaban</span></a>
+                    </li>
 
             </ul>
         </div>
@@ -324,24 +334,7 @@
             </ul>
         </div>
 
-        <a class="nav-link nav-link-cs collapsed text-truncate list-group-item custom-sidebar list-group-item-action custom-active-color {{ request()->is('admin/answer_choice_categories/*') ? 'active custom-active-color' : '' }}"
-            href="#answer_choice_categories" data-toggle="collapse" data-target="#answer_choice_categories">
-            <span class="d-none d-sm-inline"></span>Kategori Jawaban</a>
-        <div class="collapse" id="answer_choice_categories" aria-expanded="false">
-            <ul class="flex-column pl-2 nav">
-                <li class="nav-item">
-                    <a href="{{ route('admin-answercategory') }}"
-                        class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('answer_choice_categories') ? 'active custom-active-color' : '' }}"><span>
-                            Daftar Kategori Jawaban</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('admin-create-answercategory') }}"
-                        class="list-group-item  custom-sidebar list-group-item-action custom-active-color {{ request()->is('familygroup') ? 'active custom-active-color' : '' }}"><span>
-                            Buat Kategori Jawaban</span></a>
-                </li>
 
-            </ul>
-        </div>
 
 
         {{-- <a
