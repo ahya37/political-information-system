6e133c5fefae5f92f9c0fb62c2c4809960139737
ahya37 <ahmadyaniid37@gmail.com>
2023-07-07 13:34:30 +0700

    Revisi Model Pertanyaan Kuisioner

--- resources/views/pages/admin/questionnaire_questions/create.blade.php
+++ resources/views/pages/admin/questionnaire_questions/create.blade.php
@@ -13,63 +13,54 @@
                 <div class="row">
                     <div class="col-md-12 col-sm-12">
                         @include('layouts.message')
-                        <form action="{{ route('admin-questionnairequestion-store', $id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('admin-questionnairequestion-store', $id) }}" method="POST"
+                            enctype="multipart/form-data">
                             @csrf
                             <div class="card border">
                                 <div class="card-body">
-                              
-                                            <div class="form-group">
-                                                <div class="form-group">
-                                                    <label>No.</label>
-                                                    <input type="text" name="number" class="form-control col-sm-3" value="{{ $number }}" />
-                                                </div>
-                                                
-                                                
-                                                
-                                                        <label>Pilihan</label>
-                                                        <input type="text" name="pilihan" required
-                                                            class="form-control"/>
-                                                    </div>
-                                                    <div>
-                                                      <label>Jawaban</label>
-                                                      @foreach ($dataAnswer as $data)
-                                                      <div class="form-check">
-                                                          <input class="form-check-input" name="jawaban[]" type="checkbox" value="{{ $data->id }}" id="defaultCheck1">
-                                                          <label class="form-check-label" for="defaultCheck1">
-                                                            {{ $data->name }}
-                                                          </label>
-                                                        </div>
-                                                
-                                                      @endforeach
+                                    <div class="form-group">
+                                        <div class="form-group">
+                                            <label>No.</label>
+                                            <input type="text" name="number" class="form-control col-sm-3"
+                                                value="{{ $number }}" />
+                                        </div>
+                                        <label>Pilihan</label>
+                                        <input type="text" name="pilihan" required class="form-control" />
+                                    </div>
+                                    <div>
+                                        <label>Jawaban</label>
+                                        @foreach ($dataAnswer as $data)
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="jawaban[]" type="checkbox"
+                                                    value="{{ $data->id }}" id="defaultCheck1">
+                                                <label class="form-check-label" for="defaultCheck1">
+                                                    {{ $data->name }}
+                                                </label>
                                             </div>
-                                            
-                                            <div class="form-group">
-                                                <button type="submit"
-                                                    class="btn btn-sc-primary text-white  btn-sm w-00 mt-4 float-right">
-                                                    Simpan
-                                                </button>
-                                            </div>
-                                      
+                                        @endforeach
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit"
+                                            class="btn btn-sc-primary text-white  btn-sm w-00 mt-4 float-right">
+                                            Simpan
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div class="card border" style="margin-top: 100px">
+                        </form>
+                        <form action="" method="post" enctype="multipart/form-data">
+                            <div class="card border" style="margin-top: 75px">
                                 <div class="card-body">
-                                            <div class="form-group">
-                                                        <label>Pertanyaan Essay</label>
-                                                        <input type="text" name="essay" required
-                                                            class="form-control"/>
-                                                  
-                                                   
-                                            </div>
-                                            
-                                            <div class="form-group">
-                                                <button type="submit"
-                                                    class="btn btn-sc-primary text-white  btn-sm w-00 mt-4 float-right">
-                                                    Simpan
-                                                </button>
-                                            </div>
-                                      
+                                    <div class="form-group">
+                                        <label>Pertanyaan Essay</label>
+                                        <input type="text" name="essay" required class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit"
+                                            class="btn btn-sc-primary text-white  btn-sm w-00 mt-4 float-right">
+                                            Simpan
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </form>
